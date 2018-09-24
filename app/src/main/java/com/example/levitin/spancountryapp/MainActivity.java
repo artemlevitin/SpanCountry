5f8f9462bff670ffd96ca24f16c020a3621a25bb
artemlevitin <levitin@live.com>
2018-09-24 22:04:28 +0300

    change main layout and main activity

--- app/src/main/java/com/example/levitin/spancountryapp/MainActivity.java
+++ app/src/main/java/com/example/levitin/spancountryapp/MainActivity.java
@@ -2,17 +2,40 @@ package com.example.levitin.spancountryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
+import java.io.File;
+import java.io.StringWriter;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Country> countries;
+    Spinner spinner ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WorldData worldData= new WorldData();
+        WorldData worldData = new WorldData();
         InitialazerDataWorld.execute(worldData);
-       ArrayList<Country> countries = worldData.getCountries();
+        countries = worldData.getCountries();
+
     }
+
+    /*void toXml(WorldData worldData) {
+        File file = new File("C:\\file.xml");
+
+
+
+            JAXBContext jaxbContext = JAXBContext.newInstance(WorldData.class);
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+        // output pretty printed
+        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,true);
+
+        jaxbMarshaller.marshal(customer,file);
+
+        jaxbMarshaller.marshal(customer,System.out);
+        }*/
 }
